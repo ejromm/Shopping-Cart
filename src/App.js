6079e121d797b6ecae8f1008a6cff8af186815cd
Ethan Romm <ejromm@gmail.com>
2023-04-24 10:45:33 -0400

    added routing to app, pages folder and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
 export default function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <>
+    
+     <BrowserRouter>
+     <Routes>
+     <Route  path='/' element={<Header />} />
+     <Route  path='/home' element={<Home />}/>
+     <Route  path='/shop' element={<Shop />} />
+     <Route  path='/cart' element={<Cart />}/>
+     </Routes>
+     </BrowserRouter>
+     </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import NavBar from './NavBar';
+
+export default function Header() {
+
+
+    return (
+        <>
+         <div className='header'>; 
+          <span>Store</span>
+         </div>
+         <NavBar />
+        </>
+    )
+
+}
