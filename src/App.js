9e9a283f681223a558290958271cd77feb7ded0b
Ethan Romm <ejromm@gmail.com>
2023-04-26 01:33:39 -0400

    added fake store api call to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -6,6 +6,21 @@ import Shop from './pages/Shop';
 import Cart from './pages/Cart';
 
 export default function App() {
+  const [inventory, setInventory] = useState([]);
+  
+  useEffect(() => {
+    const retreiveInventory = async () => {
+      try{
+       const response = await fetch('https://fakestoreapi.com/products'); 
+       const data = await response.json(); 
+       setInventory(data);
+      } catch(error) {
+           console.log(error);
+      }
+   }
+    retreiveInventory();
+  }, []); 
+  console.log('inventory', inventory);
   return (
     <>
     
