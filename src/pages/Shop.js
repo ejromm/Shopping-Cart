5a06d0cd59e7dbac309d8245b3503611c478ec10
Ethan Romm <ejromm@gmail.com>
2023-04-26 20:39:14 -0400

    converted ratings to stars

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,16 +1,39 @@
 import React from 'react'; 
 import '../styles/Shop.css';
+import { FaStar, FaStarHalf,  } from "react-icons/fa";
+
 
 export default function Shop({inventory, setInventory}) {
         console.log('inventory in shop', inventory);
+
+    function getStars(rating) {
+        //round to nearest half
+        rating = Math.round(rating * 2 ) / 2;
+        let output = []; 
+        console.log('rating', rating)
+       
+        //append all whole stars
+        for(let i = rating ; i >= 1; i--) {
+            output.push(<FaStar style={{color: 'gold'}} />)
+            rating--; 
+            console.log('rating --', rating)
+        }
+        if (rating === .5) output.push(<FaStarHalf style={{color: "gold"}} />)
+            
+
+            return output;
+        
+    }
+   
     return (
         <div className='store'>
                 {inventory.map((item) => {
+                    
                     return (
                         <div id='shop-item'>
                             <img id='item-img' alt={item.title} src={item.image}/>
                             <span id='item-name'>{item.title.replace(/\s+/g,' ').trim()}</span>
-                            <span id='item-rating'>{item.rating.rate} / 5</span>
+                            <span id='item-rating'>{getStars(item.rating.rate)}</span>
                             <div className='item-price-cart'>
                                 <span id='item-price'>${item.price}</span>
                                 <button id='add-to-cart'>ADD TO CART</button>
